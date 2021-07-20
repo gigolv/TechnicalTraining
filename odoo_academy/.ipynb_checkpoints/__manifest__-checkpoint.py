ed642cedf1dead19322292a03dcdfb6285e98bba
Rodrigo Lozano <rlozanodoo@gmail.com>
2021-07-20 07:18:10 +0000

    [ADD] Basic Module

---
+++ odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Accademy',
+    'summary': """Academy app to manage Training""",
+    'description': """
+        Academy Module to manage training:
+        -Courses
+        -Sessions
+        -Attendees
+    """,
+    'author': 'Rodrigo Lozano',
+    'website': 'https://www.odoo.com',
+    'category': 'Training',
+    'version': '0.1',
+    'depends': ['base'],
+    'data':[
+        
+    ],
+    'demo':[
+        
+    ],
+}
