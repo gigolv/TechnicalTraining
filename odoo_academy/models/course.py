5ed704c79abd72ddb974a16b0f58dc13cf4b5c2c
Rodrigo Lozano <rlozanodoo@gmail.com>
2021-07-20 07:46:20 +0000

    [ADD] Basic Module

--- odoo_academy/models/course.py
+++ odoo_academy/models/course.py
@@ -4,5 +4,5 @@ from odoo import models, fields, api
 
 class Course(models.Model):
     
-    _name ="academy.course"
-    _description ="Course Info"
+    _name ='academy.course'
+    _description ='Course Info'
